bb4f5fe631dd35f6e697a0e7da864e471d7ca966
Gabriel Yuji Yasuda Cardoso <gabrielyyasuda@gmail.com>
2023-02-24 11:11:18 -0300

    Commit

--- src/components/NewTodoForm.js
+++ src/components/NewTodoForm.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NewTodoForm() {
+  const [description, setDescription] = useState("");
+  const [assigned, setAssigned] = useState("");
+
+  //const assignedChange = (event) => {
+  //  console.log("assigned", event.target.value);
+  //  setAssigned(event.target.value);
+  //};
+
+  //const descriptionChange = (event) => {
+  //  console.log("description", event.target.value);
+  //  setDescription(event.target.value);
+  //};
+
   return (
     <div className="mt-5">
       <form>
         <div className="mb-3">
           <label className="form-label">Assigned</label>
-          <input type="text" className="form-control" required></input>
+          <input
+            type="text"
+            className="form-control"
+            required
+            onChange={(e) => setAssigned(e.target.value)}
+            value={assigned}
+          ></input>
         </div>
 
         <div className="mb-3">
           <label className="form-label"></label>
-          <textarea className="form-control" rows={3} required></textarea>
+          <textarea
+            className="form-control"
+            rows={3}
+            required
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+          ></textarea>
         </div>
 
         <button type="button" className="btn btn-primary mt-3">
